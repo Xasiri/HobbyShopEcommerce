c0c04e401e31acaaba0247709a32ce97b936f444
asiri <asiri1234@gmail.com>
2021-02-05 12:25:14 +0530

    commit deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScrenn from './screens/HomeScrenn';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome To HobbyShop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/product/:id" component={ProductScreen} />
+
+          <Route path="/" component={HomeScrenn} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/HomeScrenn.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import axios from 'axios';
+import Product from '../components/Product';
+
+const HomeScrenn = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default HomeScrenn;
